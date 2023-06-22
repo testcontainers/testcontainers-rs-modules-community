e1b21078780a93ecf616f73426ff5def27e6c1c6
Artem Medvedev <i@ddtkey.com>
2023-06-23 01:41:51 +0200

    feat: init previously existed modules within `testcontainers-rs` (#1)
    All modules removed in
    https://github.com/testcontainers/testcontainers-rs/pull/473 were
    extracted into this crate on feature-based approach.
    
    Other notes:
    - all deps are updated + minor fixes to make it working
    - initial CI config + conventional commits check 
    - version is attached to `testcontainers-rs` (with `beta` flag for now)
    - some unnecessary/similar deps are removed

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # testcontainers-rs-modules-community
-Community maintained modules for Testcontainers for Rust
+
+![Continuous Integration](https://github.com/testcontainers/testcontainers-rs-modules-community/workflows/Continuous%20Integration/badge.svg?branch=main)
+[![Crates.io](https://img.shields.io/crates/v/testcontainers-modules.svg)](https://crates.io/crates/testcontainers-modules)
+[![Docs.rs](https://docs.rs/testcontainers-modules/badge.svg)](https://docs.rs/testcontainers-modules)
+
+Community maintained modules for Testcontainers for Rust.
+
+## Usage
+
+1. Depend on `testcontainers-modules` with necessary features (e.g `postgres`, `minio` and etc)
+2. Use provided modules in accordance with [testcontainers-rs](https://github.com/testcontainers/testcontainers-rs)
+
+## License
+
+- MIT license ([LICENSE](LICENSE) or http://opensource.org/licenses/MIT)
+
---
+++ src/parity_parity/mod.rs
@@ -0,0 +1,75 @@
+use testcontainers::{core::WaitFor, Image, ImageArgs};
+
+const NAME: &str = "parity/parity";
+const TAG: &str = "v2.5.0";
+
+#[derive(Debug, Default)]
+pub struct ParityEthereum;
+
+#[derive(Debug, Default, Clone)]
+pub struct ParityEthereumArgs;
+
+impl ImageArgs for ParityEthereumArgs {
+    fn into_iterator(self) -> Box<dyn Iterator<Item = String>> {
+        Box::new(
+            vec![
+                "--config=dev".to_string(),
+                "--jsonrpc-apis=all".to_string(),
+                "--unsafe-expose".to_string(),
+                "--tracing=on".to_string(),
+            ]
+            .into_iter(),
+        )
+    }
+}
+
+impl Image for ParityEthereum {
+    type Args = ParityEthereumArgs;
+
+    fn name(&self) -> String {
+        NAME.to_owned()
+    }
+
+    fn tag(&self) -> String {
+        TAG.to_owned()
+    }
+
+    fn ready_conditions(&self) -> Vec<WaitFor> {
+        vec![WaitFor::message_on_stderr("Public node URL:")]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use testcontainers::clients;
+
+    use crate::parity_parity;
+
+    #[test]
+    fn parity_parity_net_version() {
+        let _ = pretty_env_logger::try_init();
+        let docker = clients::Cli::default();
+        let node = docker.run(parity_parity::ParityEthereum::default());
+        let host_port = node.get_host_port_ipv4(8545);
+
+        let response = reqwest::blocking::Client::new()
+            .post(format!("http://127.0.0.1:{host_port}"))
+            .body(
+                serde_json::json!({
+                    "jsonrpc": "2.0",
+                    "method": "net_version",
+                    "params": [],
+                    "id": 1
+                })
+                .to_string(),
+            )
+            .header("content-type", "application/json")
+            .send()
+            .unwrap();
+
+        let response = response.text().unwrap();
+        let response: serde_json::Value = serde_json::from_str(&response).unwrap();
+
+        assert_eq!(response["result"], "17");
+    }
+}
