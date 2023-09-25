834eb3dfebaa3dcdb33e01542ba2d9ecbce886b0
Artem Medvedev <artem.medvedev@workato.com>
2023-09-25 17:20:00 +0200

    docs: align readme & documentation, add an example

---
+++ examples/postgres.rs
@@ -0,0 +1,21 @@
+use testcontainers_modules::{postgres::Postgres, testcontainers::clients::Cli};
+
+fn main() {
+    // startup the module
+    let docker = Cli::default();
+    let node = docker.run(Postgres::default());
+
+    // prepare connection string
+    let connection_string = &format!(
+        "postgres://postgres:postgres@127.0.0.1:{}/postgres",
+        node.get_host_port_ipv4(5432)
+    );
+    // container is up, you can use it
+    let mut conn = postgres::Client::connect(connection_string, postgres::NoTls).unwrap();
+    let rows = conn.query("SELECT 1 + 1", &[]).unwrap();
+    assert_eq!(rows.len(), 1);
+
+    let first_row = &rows[0];
+    let first_column: i32 = first_row.get(0);
+    assert_eq!(first_column, 2);
+}
