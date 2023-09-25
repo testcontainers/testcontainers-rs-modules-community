c9509632dc818788729a666cecc70ac16ad2665d
Artem Medvedev <artem.medvedev@workato.com>
2023-09-25 16:22:19 +0200

    refactor(zookeper): use bitnami image
    Additionally it solves the issues with unreliable testing, so I've enabled it.

--- src/zookeeper/mod.rs
+++ src/zookeeper/mod.rs
@@ -1,10 +1,23 @@
+use std::collections::HashMap;
+
 use testcontainers::{core::WaitFor, Image};
 
-const NAME: &str = "zookeeper";
-const TAG: &str = "3.6.2";
+const NAME: &str = "bitnami/zookeeper";
+const TAG: &str = "3.9.0";
 
-#[derive(Debug, Default)]
-pub struct Zookeeper;
+#[derive(Debug)]
+pub struct Zookeeper {
+    env_vars: HashMap<String, String>,
+}
+
+impl Default for Zookeeper {
+    fn default() -> Self {
+        let mut env_vars = HashMap::new();
+        env_vars.insert("ALLOW_ANONYMOUS_LOGIN".to_owned(), "yes".to_owned());
+
+        Self { env_vars }
+    }
+}
 
 impl Image for Zookeeper {
     type Args = ();
@@ -18,7 +31,14 @@ impl Image for Zookeeper {
     }
 
     fn ready_conditions(&self) -> Vec<WaitFor> {
-        vec![WaitFor::message_on_stdout("Started AdminServer")]
+        vec![
+            WaitFor::message_on_stdout("Started AdminServer"),
+            WaitFor::message_on_stdout("PrepRequestProcessor (sid:0) started"),
+        ]
+    }
+
+    fn env_vars(&self) -> Box<dyn Iterator<Item = (&String, &String)> + '_> {
+        Box::new(self.env_vars.iter())
     }
 }
 
@@ -32,12 +52,11 @@ mod tests {
     use crate::zookeeper::Zookeeper as ZookeeperImage;
 
     #[test]
-    #[ignore]
     fn zookeeper_check_directories_existence() {
         let _ = pretty_env_logger::try_init();
 
         let docker = clients::Cli::default();
-        let node = docker.run(ZookeeperImage);
+        let node = docker.run(ZookeeperImage::default());
 
         let host_port = node.get_host_port_ipv4(2181);
         let zk_urls = format!("127.0.0.1:{host_port}");
