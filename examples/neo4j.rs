c0ed62a6bd1a1b0e699be3356c705984b4ae798a
Paul Horn <paul.horn@neo4j.com>
2023-11-14 14:42:50 +0100

    feat: Add Neo4j example

---
+++ examples/neo4j.rs
@@ -0,0 +1,31 @@
+use testcontainers_modules::{neo4j::Neo4j, testcontainers::clients::Cli};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let docker = Cli::default();
+    let container = docker.run(Neo4j::default());
+
+    // prepare neo4rs client
+    let config = neo4rs::ConfigBuilder::new()
+        .uri(container.image().bolt_uri_ipv4())
+        .user(container.image().user().expect("default user is set"))
+        .password(
+            container
+                .image()
+                .password()
+                .expect("default password is set"),
+        )
+        .build()?;
+
+    // connect ot Neo4j
+    let graph = neo4rs::Graph::connect(config).await?;
+
+    // run a test query
+    let mut rows = graph.execute(neo4rs::query("RETURN 1 + 1")).await?;
+    while let Some(row) = rows.next().await? {
+        let result: i64 = row.get("1 + 1").unwrap();
+        assert_eq!(result, 2);
+    }
+
+    Ok(())
+}
