07142bdb75e8dad24fb581b9cf22c52aac67ed53
Jean-Pierre Geslin <jpgeslin@hotmail.fr>
2024-01-24 22:28:03 +0100

    feat: add `SurrealDb` module (#87)

---
+++ examples/surrealdb.rs
@@ -0,0 +1,74 @@
+use serde::{Deserialize, Serialize};
+use surrealdb::{
+    engine::remote::ws::{Client, Ws},
+    opt::auth::Root,
+    Surreal,
+};
+use testcontainers_modules::{
+    surrealdb::{SurrealDb, SURREALDB_PORT},
+    testcontainers::clients::Cli,
+};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Name {
+    first: String,
+    last: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Person {
+    title: String,
+    name: Name,
+    marketing: bool,
+}
+
+#[tokio::main]
+async fn main() {
+    let _ = pretty_env_logger::try_init();
+    let docker = Cli::default();
+    let node = docker.run(SurrealDb::default());
+    let url = format!("127.0.0.1:{}", node.get_host_port_ipv4(SURREALDB_PORT));
+
+    let db: Surreal<Client> = Surreal::init();
+    db.connect::<Ws>(url)
+        .await
+        .expect("Failed to connect to SurrealDB");
+    db.signin(Root {
+        username: "root",
+        password: "root",
+    })
+    .await
+    .expect("Failed to signin to SurrealDB");
+
+    db.use_ns("test")
+        .use_db("test")
+        .await
+        .expect("Failed to use test db");
+
+    db.create::<Option<Person>>(("person", "tobie"))
+        .content(Person {
+            title: "Founder & CEO".to_string(),
+            name: Name {
+                first: "Tobie".to_string(),
+                last: "Morgan Hitchcock".to_string(),
+            },
+            marketing: true,
+        })
+        .await
+        .expect("Failed to create Tobie :(");
+
+    let result = db
+        .select::<Option<Person>>(("person", "tobie"))
+        .await
+        .expect("Failed to select Tobie :(");
+
+    assert!(result.is_some());
+    let result = result.expect("Failed to unwrap Tobie :(");
+
+    assert_eq!(result.title, "Founder & CEO");
+    assert_eq!(result.name.first, "Tobie");
+    assert_eq!(result.name.last, "Morgan Hitchcock");
+    assert_eq!(result.marketing, true);
+
+    println!("All right, all right, all right!\n\n{:#?}", result);
+}
