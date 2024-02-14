c2816ca8847afeb898ff2e29f9ece11d13f98830
Cristian Kubis <cristian.kubis@tsunix.de>
2024-02-14 16:20:41 +0100

    Add module level documentation
    Signed-off-by: Cristian Kubis <github@tsunix.de>

--- src/solr/mod.rs
+++ src/solr/mod.rs
@@ -2,9 +2,34 @@ use std::collections::HashMap;
 
 use testcontainers::{core::WaitFor, Image};
 
+pub const SOLR_PORT: u16 = 8983;
+
 const NAME: &str = "solr";
 const TAG: &str = "9.5.0-slim";
 
+/// Module to work with [`Solr`] inside of tests.
+///
+/// Starts an instance of Solr based on the official [`Solr docker image`].
+///
+/// By default Solr is exposed via HTTP on Port 8983 ([`SOLR_PORT`]) and has no access control. Please refer to the [`Solr reference guide`] for more informations on how to interact with the API.
+///
+/// # Example
+/// ```
+/// use testcontainers::clients;
+/// use testcontainers_modules::solr;
+///
+/// let docker = clients::Cli::default();
+/// let solr_instance = docker.run(solr::Solr::default());
+/// let host_port = solr_instance.get_host_port_ipv4(solr::SOLR_PORT);
+
+/// let solr_url = format!("http://127.0.0.1:{}", host_port);
+///
+/// // use HTTP client to interact with the solr API
+/// ```
+///
+/// [`Solr`]: https://solr.apache.org/
+/// [`Solr docker image`]: https://hub.docker.com/_/solr
+/// [`Solr reference guide`]: https://solr.apache.org/guide/solr/latest/
 #[derive(Debug, Default)]
 pub struct Solr {
     env_vars: HashMap<String, String>,
@@ -42,7 +67,7 @@ mod tests {
         let docker = clients::Cli::default();
         let solr_image = Solr::default();
         let container = docker.run(solr_image);
-        let host_port = container.get_host_port_ipv4(8983);
+        let host_port = container.get_host_port_ipv4(SOLR_PORT);
 
         let url = format!("http://localhost:{}/solr/admin/cores?action=STATUS", host_port);
         let res = reqwest::blocking::get(url)
