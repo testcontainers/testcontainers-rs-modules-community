53c937cd0a5850eb652c8fd83c33d5a890498fc4
Philippe Laflamme <philippe@hopper.com>
2024-04-09 11:05:26 -0400

    feat: bump google-cloud-sdk

--- src/google_cloud_sdk_emulators/mod.rs
+++ src/google_cloud_sdk_emulators/mod.rs
@@ -1,7 +1,7 @@
 use testcontainers::{core::WaitFor, Image, ImageArgs};
 
-const NAME: &str = "google/cloud-sdk";
-const TAG: &str = "362.0.0-emulators";
+const NAME: &str = "gcr.io/google.com/cloudsdktool/google-cloud-cli";
+const TAG: &str = "471.0.0-emulators";
 
 const HOST: &str = "0.0.0.0";
 pub const BIGTABLE_PORT: u16 = 8086;
