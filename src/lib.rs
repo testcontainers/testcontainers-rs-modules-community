230540f76f916ef68dd4c338e477258e2dee8ab5
Flavio Prado <flavioprado@MacBook-Pro-2.local>
2024-04-21 07:17:12 +0300

    feat: add support for KWOK cluster image

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,9 @@ pub mod google_cloud_sdk_emulators;
 #[cfg(feature = "kafka")]
 #[cfg_attr(docsrs, doc(cfg(feature = "kafka")))]
 pub mod kafka;
+#[cfg(feature = "kwok_cluster")]
+#[cfg_attr(docsrs, doc(cfg(feature = "kwok_cluster")))]
+pub mod kwok_cluster;
 #[cfg(feature = "localstack")]
 #[cfg_attr(docsrs, doc(cfg(feature = "localstack")))]
 pub mod localstack;
