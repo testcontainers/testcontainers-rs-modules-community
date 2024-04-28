bca942f0af1f4f393a818ae132cc83c4b825415e
hadi <hadi.abou.mrad@gmail.com>
2024-04-28 22:23:16 +0200

    pr review, simplify code

--- src/nats/mod.rs
+++ src/nats/mod.rs
@@ -1,113 +1,23 @@
-use std::borrow::Cow;
+use testcontainers::{core::WaitFor, Image};
 
-use testcontainers::{core::WaitFor, Image, RunnableImage};
+const NAME: &str = "nats";
+const TAG: &str = "2.10.14";
 
 /// Nats image for [testcontainers](https://crates.io/crates/testcontainers).
 ///
 /// This image is based on the official [Nats](https://hub.docker.com/_/nats) image.
-/// The default user is `` and the default password is ``.
+#[derive(Debug, Default)]
+pub struct Nats;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Nats {
-    version: Value,
-    user: Option<Value>,
-    pass: Option<Value>,
-}
-
-impl Nats {
-    const DEFAULT_USER: &'static str = "";
-    const DEFAULT_PASS: &'static str = "";
-    const DEFAULT_VERSION_TAG: &'static str = "2.10.14";
-
-    /// Create a new instance of a Nats image.
-    #[must_use]
-    pub fn new() -> Self {
-        Self {
-            version: Cow::Borrowed(Self::DEFAULT_VERSION_TAG),
-            user: Some(Cow::Borrowed(Self::DEFAULT_USER)),
-            pass: Some(Cow::Borrowed(Self::DEFAULT_PASS)),
-        }
-    }
-
-    /// Set the username to use.
-    #[must_use]
-    pub fn with_user(mut self, user: impl Into<Value>) -> Self {
-        self.user = Some(user.into());
-        self
-    }
-
-    /// Set the password to use.
-    #[must_use]
-    pub fn with_password(mut self, pass: impl Into<Value>) -> Self {
-        self.pass = Some(pass.into());
-        self
-    }
-
-    /// Do not use any authentication on the testcontainer.
-    ///
-    /// Setting this will override any prior usages of [`Self::with_user`] and
-    /// [`Self::with_password`].
-    pub fn without_authentication(mut self) -> Self {
-        self.user = None;
-        self.pass = None;
-        self
-    }
-}
-
-type Value = Cow<'static, str>;
-
-impl Default for Nats {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-/// The actual Nats testcontainers image type which is returned by `container.image()`
-pub struct NatsImage {
-    version: String,
-    auth: Option<(String, String)>,
-}
-
-impl NatsImage {
-    /// Return the version of the Nats image.
-    #[must_use]
-    pub fn version(&self) -> &str {
-        &self.version
-    }
-
-    /// Return the user/password authentication tuple of the Nats server.
-    /// If no authentication is set, `None` is returned.
-    #[must_use]
-    pub fn auth(&self) -> Option<(&str, &str)> {
-        self.auth
-            .as_ref()
-            .map(|(user, pass)| (user.as_str(), pass.as_str()))
-    }
-
-    /// Return the user of the Nats server.
-    /// If no authentication is set, `None` is returned.
-    #[must_use]
-    pub fn user(&self) -> Option<&str> {
-        self.auth().map(|(user, _)| user)
-    }
-
-    /// Return the password of the Nats server.
-    /// If no authentication is set, `None` is returned.
-    #[must_use]
-    pub fn password(&self) -> Option<&str> {
-        self.auth().map(|(_, pass)| pass)
-    }
-}
-
-impl Image for NatsImage {
+impl Image for Nats {
     type Args = ();
 
     fn name(&self) -> String {
-        "nats".to_owned()
+        NAME.to_owned()
     }
 
     fn tag(&self) -> String {
-        self.version.clone()
+        TAG.to_owned()
     }
 
     fn ready_conditions(&self) -> Vec<WaitFor> {
@@ -118,92 +28,23 @@ impl Image for NatsImage {
     }
 }
 
-impl Nats {
-    pub fn build(self) -> NatsImage {
-        let auth = self
-            .user
-            .and_then(|user| self.pass.map(|pass| (user.into_owned(), pass.into_owned())));
-
-        let version = self.version.into_owned();
-
-        NatsImage { version, auth }
-    }
-}
-
-impl From<Nats> for NatsImage {
-    fn from(nats: Nats) -> Self {
-        nats.build()
-    }
-}
-
-impl From<Nats> for RunnableImage<NatsImage> {
-    fn from(nats: Nats) -> Self {
-        Self::from(nats.build())
-    }
-}
-
-impl std::fmt::Debug for NatsImage {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("NatsImage")
-            .field("version", &self.version)
-            .field("auth", &self.auth())
-            .finish()
-    }
-}
-
 #[cfg(test)]
 mod tests {
+    use crate::nats::Nats;
     use futures::StreamExt;
     use testcontainers::runners::AsyncRunner;
 
-    use super::*;
-
-    #[test]
-    fn set_user() {
-        let nats = Nats::new().with_user("shalbaal").build();
-        assert_eq!(nats.user(), Some("shalbaal"));
-        assert_eq!(nats.auth(), Some(("shalbaal", "")));
-    }
-
-    #[test]
-    fn set_password() {
-        let nats = Nats::new().with_password("Passwort").build();
-        assert_eq!(nats.password(), Some("Passwort"));
-        assert_eq!(nats.auth(), Some(("", "Passwort")));
-    }
-
-    #[test]
-    fn set_short_password() {
-        let nats = Nats::new().with_password("1337").build();
-        assert_eq!(nats.password(), Some("1337"));
-        assert_eq!(nats.auth(), Some(("", "1337")));
-    }
-
-    #[test]
-    fn disable_auth() {
-        let nats = Nats::new().without_authentication().build();
-        assert_eq!(nats.password(), None);
-        assert_eq!(nats.user(), None);
-        assert_eq!(nats.auth(), None);
-    }
-
     #[tokio::test]
     async fn it_works() {
-        let container = Nats::default().start().await;
-
-        let auth_user = container.image().user().expect("");
-        let auth_pass = container.image().password().expect("");
+        let container = Nats.start().await;
 
         let host_port = container.get_host_port_ipv4(4222).await;
         let url = format!("127.0.0.1:{host_port}");
 
-        let nats_client = async_nats::ConnectOptions::with_user_and_password(
-            auth_user.to_string(),
-            auth_pass.to_string(),
-        )
-        .connect(url)
-        .await
-        .expect("failed to connect to nats server");
+        let nats_client = async_nats::ConnectOptions::default()
+            .connect(url)
+            .await
+            .expect("failed to connect to nats server");
 
         let mut subscriber = nats_client
             .subscribe("messages")
