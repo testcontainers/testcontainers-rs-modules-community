06efbb0474d1718efa459c718eff885e9354e833
Neil Hansen <51208969+neilyio@users.noreply.github.com>
2024-05-10 13:05:50 -0700

    docs(localstack): fix localstack example by setting `force_path_style` (#132)
    Closes #131, where I discovered the `localstack` example is broken when
    running:
    ```
    cargo run --example localstack --features="localstack" 
    ```
    
    Some research brought me to
    [awslabs/aws-sdk-rust#1138](https://github.com/awslabs/aws-sdk-rust/issues/1138),
    which uncovered that `create_bucket` is broken here because `localstack`
    uses the soon-to-be-deprecated
    ["path-style"](https://aws.amazon.com/blogs/aws/amazon-s3-path-deprecation-plan-the-rest-of-the-story/)
    address style rather than the newer "virutal-hosted" style.
    
    I reworked the configuration in the example to set
    `force_path_style(true)`, which fixes `create_bucket`. The example now
    runs successfully.
    
    I also updated the `BehaviorVersion`, because the currently referenced
    one is now deprecated and was causing a warning.

--- examples/localstack.rs
+++ examples/localstack.rs
@@ -1,4 +1,4 @@
-use aws_config::{meta::region::RegionProviderChain, BehaviorVersion};
+use aws_config::{BehaviorVersion, Region};
 use aws_sdk_s3 as s3;
 use testcontainers_modules::{
     localstack::LocalStack,
@@ -8,25 +8,27 @@ use testcontainers_modules::{
 #[tokio::main]
 #[allow(clippy::result_large_err)]
 async fn main() -> Result<(), s3::Error> {
+    let _ = pretty_env_logger::try_init();
+
     let image: RunnableImage<LocalStack> =
         RunnableImage::from(LocalStack).with_env_var(("SERVICES", "s3"));
     let container = image.start().await;
 
     let host_ip = container.get_host().await;
     let host_port = container.get_host_port_ipv4(4566).await;
-
     // Set up AWS client
     let endpoint_url = format!("http://{host_ip}:{host_port}");
-    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
     let creds = s3::config::Credentials::new("fake", "fake", None, None, "test");
-    let config = aws_config::defaults(BehaviorVersion::v2023_11_09())
-        .region(region_provider)
+
+    let config = aws_sdk_s3::config::Builder::default()
+        .behavior_version(BehaviorVersion::v2024_03_28())
+        .region(Region::new("us-east-1"))
         .credentials_provider(creds)
         .endpoint_url(endpoint_url)
-        .load()
-        .await;
+        .force_path_style(true)
+        .build();
 
-    let client = s3::Client::new(&config);
+    let client = s3::Client::from_conf(config);
 
     client
         .create_bucket()
