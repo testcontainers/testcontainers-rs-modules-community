fd713440f839ad6b88a45930ba21bdda06e091f7
Frank Elsinga <frank@elsinga.de>
2024-09-26 02:18:08 +0800

    formatting fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 // let's document our code for other/future developers
 #![deny(missing_docs)]
-
 #![cfg_attr(docsrs, deny(rustdoc::broken_intra_doc_links))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(
